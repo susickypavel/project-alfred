cf64383cc0d77bbf6a5dcd6e7705c0e3e52fa487
Pavel Sušický <pavels.sus@gmail.com>
2021-03-06 20:41:21 +0100

    feat: configuration service

--- project-alfred/Program.cs
+++ project-alfred/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using project_alfred.Services;
@@ -11,12 +12,18 @@ namespace project_alfred
 
         private async Task MainAsync()
         {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("config.json").Build();
+            
             var services = new ServiceCollection();
+            services.AddSingleton(config);
             services.AddSingleton<BootService>();
 
             var serviceProvider = services.BuildServiceProvider();
 
             serviceProvider.GetService<BootService>().Boot();
+            
+            await Task.Delay(-1);
         }
     }
 }
--- project-alfred/src/Services/BootService.cs
+++ project-alfred/src/Services/BootService.cs
@@ -1,30 +1,38 @@
-﻿namespace project_alfred.Services
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace project_alfred.Services
 {
     public class BootService
     {
-        public BootService()
+        private readonly IConfigurationRoot _config;
+        private readonly DiscordSocketClient _client;
+        
+        public BootService(IServiceProvider provider)
         {
-            // _client = _client = new DiscordSocketClient();
-            // _client.Log += Log;
-            //
-            // _config = new ConfigurationBuilder()
-            //     .AddJsonFile("config.json").Build();
+            _config = provider.GetRequiredService<IConfigurationRoot>();
+            // TODO: extract to service
+            _client = new DiscordSocketClient();
         }
         
-        public void Boot()
+        public async void Boot()
         {
-            // var token = _config["token"];
-            //
-            // await _client.LoginAsync(TokenType.Bot, token);
-            // await _client.StartAsync();
-            //
-            // await Task.Delay(-1);
+            _client.Log += Log;
+            
+            var token = _config["token"];
+            
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
         }
         
-        // private Task Log(LogMessage msg)
-        // {
-        //     Console.WriteLine(msg.ToString());
-        //     return Task.CompletedTask;
-        // }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
     }
 }

3f6d2d3f6bf7aa50bcef0b4e74a909716a15d7d8
Pavel Sušický <pavels.sus@gmail.com>
2021-03-06 14:35:43 +0100

    feat: basic bot

--- project-alfred/Program.cs
+++ project-alfred/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace project_alfred
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();   
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            
+            var token = "TODO: implement environment variables";
+            
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }

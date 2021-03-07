225a9bb1447ee658909bdbdf1c4ae4faf1bcb48d
Pavel Sušický <pavels.sus@gmail.com>
2021-03-07 22:55:06 +0100

    refactor

---
+++ project-alfred/src/Modules/CrudCommands.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using Discord.Commands;
+
+namespace project_alfred.Modules
+{
+    public class CrudCommands : ModuleBase
+    {
+        [Command("add")]
+        public async Task AddSongRecord(string url)
+        {
+            await ReplyAsync($"TODO: implement");
+        }
+    }
+}

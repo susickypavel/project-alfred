fa93ed3a15dda31119593f25620380e5d32ce664
Pavel Sušický <pavels.sus@gmail.com>
2021-03-07 23:11:07 +0100

    feat: test database command

--- project-alfred/src/Modules/CrudCommands.cs
+++ project-alfred/src/Modules/CrudCommands.cs
@@ -1,14 +1,25 @@
 ﻿using System.Threading.Tasks;
 
 using Discord.Commands;
+using project_alfred.models;
 
 namespace project_alfred.Modules
 {
-    public class CrudCommands : ModuleBase
+    public class CrudCommands : ModuleBase<SocketCommandContext>
     {
         [Command("add")]
         public async Task AddSongRecord(string url)
         {
+            // var std = new SongRecord()
+            // {
+            //     user = Context.User.Id,
+            //     url = url
+            // };
+            //
+            // var context = new SongRecordContext();
+            // context.Add(std);
+            // await context.SaveChangesAsync();
+            
             await ReplyAsync($"TODO: implement");
         }
     }

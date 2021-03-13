31cf165218bc8088088729bdb4a5f99de069a465
Pavel Sušický <pavels.sus@gmail.com>
2021-03-13 16:06:21 +0100

    feat: improved command error logging

--- src/Services/CommandHandler.cs
+++ src/Services/CommandHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+
+using project_alfred.TypeReaders;
 
 namespace project_alfred.Services
 {
@@ -27,13 +31,47 @@ namespace project_alfred.Services
             _client.MessageReceived += OnMessageReceived;
             _command.CommandExecuted += OnCommandExecuted;
             
+            _command.AddTypeReader(typeof(Url), new UrlTypeReader());
             await _command.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
         }
 
         private async Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
         {
             var commandName = command.IsSpecified ? command.Value.Name : "A command";
-            await _logger.Log(new LogMessage(LogSeverity.Info, "CommandExecution", $"{commandName} was executed at {DateTime.UtcNow}"));
+            var severity = result.Error != null ? LogSeverity.Error : LogSeverity.Info;
+            
+            await _logger.Log(new LogMessage(severity, "CommandExecution", $"{commandName} was executed at {DateTime.UtcNow}"));
+            
+            if (!result.IsSuccess)
+            {
+                var message = new StringBuilder($"{context.User.Id} ({context.User.Username}#{context.User.DiscriminatorValue}) executed command {commandName}: ");
+                
+                switch (result.Error)
+                {
+                    case CommandError.UnknownCommand:
+                        message.Append($"Such command does not exist.");
+                        break;
+                    case CommandError.ParseFailed:
+                        message.Append($"Wrong parameter value was send.");
+                        break;
+                    case CommandError.BadArgCount:
+                        message.Append($"Wrong count of parameters was send.");
+                        break;
+                    case CommandError.ObjectNotFound:
+                        break;
+                    case CommandError.MultipleMatches:
+                        break;
+                    case CommandError.UnmetPrecondition:
+                        break;
+                    case CommandError.Exception:
+                        break;
+                    case CommandError.Unsuccessful:
+                        message.Append($"Something went wrong.");
+                        break;
+                }
+                
+                await _logger.Log(new LogMessage(LogSeverity.Debug, "CommandExecution", message.ToString()));
+            }
         }
 
         private async Task OnMessageReceived(SocketMessage msg)

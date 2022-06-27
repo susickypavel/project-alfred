f943fc9a50f486ba3a794d78f2a694724f4473f0
Pavel Sušický <susicky.pavel@outlook.cz>
2022-06-27 10:54:21 +0200

    feat(bot): add Discord dependency and boilerplate

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Discord;
+using Discord.WebSocket;
 
-Console.WriteLine("Hello, World!");
+namespace project_alfred;
+
+public class Program
+{
+    private DiscordSocketClient? _client;
+    
+    public static Task Main(string[] args)
+    {
+        return new Program().MainAsync();
+    }
+
+    private async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        
+        await _client.LoginAsync(TokenType.Bot, "TODO");
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
